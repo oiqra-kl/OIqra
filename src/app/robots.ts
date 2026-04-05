23ff7fed0837475ef142ba228d0594446146df37
izwan61 <191495262+izwanGit@users.noreply.github.com>
2026-04-05 15:48:44 +0800

    Update sitemap and robots URLs to o-iqra.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
   // Replace with actual domain later
-  const URL = 'https://oiqra.vercel.app';
+  const URL = 'https://o-iqra.com';
 
   return {
     rules: {
